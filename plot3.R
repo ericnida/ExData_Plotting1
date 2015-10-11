003aa28077de525e6fe4834a20460b1c96fae926
ericnida <nidaeric@gmail.com>
2015-10-11 17:10:25 -0400

    Committing plots 1-4 and scripts

---
+++ plot3.R
@@ -0,0 +1,19 @@
+#plot3.R
+
+unzip("exdata-data-household_power_consumption.zip")
+dataSet <- read.table(file = "household_power_consumption.txt", 
+                      na.strings = c("?", ""), sep = ";", header=TRUE)
+dataSet$Date <- as.Date(dataSet$Date, format = "%d/%m/%Y")  
+dataSet$timetemp <- paste(dataSet$Date, dataSet$Time)
+dataSet$Time <- strptime(dataSet$timetemp, format = "%Y-%m-%d %H:%M:%S")
+data <- dataSet[dataSet$Date == "2007-02-01" | dataSet$Date == "2007-02-02",]
+
+png(filename="plot3.png", width = 480, height = 480)
+plot(data$Time, data$Sub_metering_1, type="l", 
+     xlab="", ylab="Energy sub metering")
+lines(data$Time, data$Sub_metering_2, type="l", col="red")
+lines(data$Time, data$Sub_metering_3, type="l", col="blue")
+legend("topright", lty=1, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col=c("black", "red", "blue"))
+dev.off()
